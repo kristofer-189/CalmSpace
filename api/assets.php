b964f50c233b5950955e3f7c981304d1d0569fb0
kristofer-189 <kitto189@gmail.com>
2024-12-27 22:17:57 +0700

    Update assets.php

--- api/assets.php
+++ api/assets.php
@@ -1,3 +1,9 @@
 <?php  
 
-require __DIR__ . "/../public/" . basename($_GET['type']) . "/" . basename($_GET['file']);
+// require __DIR__ . "/../public/" . basename($_GET['type']) . "/" . basename($_GET['file']);
+if ($_GET['type'] === 'css'){
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . "/../public/css/" . basename($_GET['file']);
+} else {
+    echo require __DIR__ . "/../public/" . basename($_GET['type']) . "/" . basename($_GET['file']);
+}
